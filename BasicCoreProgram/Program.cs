ebb1e5e953556a42f771cc3e77f027528f3e7436
faheemjailor <faheemjailor0055>
2021-11-24 19:48:01 +0530

    Faheem UC4 for largest number

--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -2,22 +2,33 @@
 
 namespace BasicCoreProgramming
 {
-    class HarmonicNumber
+    class LargestNumber
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            double sum = 0;
+            Console.WriteLine("Enter Three Numbers : ");
+            Console.Write("\nEnter Number 1 : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter number of terms : ");
-            int num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter Number 2 : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("\nHarmonic series is    ");
-            for (int i = 1; i <= num; i++)
+            Console.Write("\nEnter Number 3 : ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2 && num1 > num3)
             {
-                Console.Write("1/{0} + ", i);
-                sum += 1 / (float)i;
+                Console.WriteLine("\nLargest number is : " + num1);
             }
-            Console.WriteLine("\n\nsum of harmonic series upto {0} terms : {1}", num, sum);
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine("\nLargest number is : " + num2);
+            }
+            else
+            {
+                Console.WriteLine("\nLargest number is : " + num3);
+            }
+
         }
     }
 }
