4d391e2356e698f78207c8d464bc23f872c549fe
faheemjailor <faheemjailor0055>
2021-11-24 19:39:45 +0530

    Faheem UC1 for finding whether number is even or odd

---
+++ BasicCoreProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BasicCoreProgramming
+{
+    class EvenOrOdd
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number : ");
+            //Convert user input into integer
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Number is Even");
+            }
+            else
+            {
+                Console.WriteLine("Number is Odd");
+            }
+        }
+    }
+}
