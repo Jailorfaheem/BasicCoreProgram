56108cb1d0b9a93c7818fe7306302429c8966ef5
faheemjailor <faheemjailor0055>
2021-11-24 20:45:31 +0530

    Faheem UC9 for vowels and consonants

--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -2,26 +2,23 @@
 
 namespace BasicCoreProgramming
 {
-    class SwapNumber
+    class VowelsOrConsonant
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Two Nubers :");
-            Console.Write("\nEnter Number 1 : ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            //taking input from user
+            Console.WriteLine("Enter an Alphabet :");
+            char ch = Convert.ToChar(Console.ReadLine()); //converted to chararcter
 
-            Console.Write("Enter number 2 : ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-
-            //Swapping numbers
-            int temp = num1;
-            num1 = num2;
-            num2 = temp;
-
-            Console.WriteLine("\n AFTER SWAPPING NUMBER");
-            Console.WriteLine("\nFirst Number :" + num1);
-            Console.WriteLine("Second Number :" + num2);
-
+            //if else condition for checking of vowels or consonants
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine("{0} is a Vowel", ch);
+            }
+            else
+            {
+                Console.WriteLine("{0} is a Consonant", ch);
+            }
         }
     }
 }
